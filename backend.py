aeccaeb603b445bc0c40bffb5b9d9676643e67db
MACIEKz8 <maciej.zawisz@student.pk.edu.pl>
2021-06-08 23:10:33 +0200

    Dodanie doc_strings

--- backend.py
+++ backend.py
@@ -5,7 +5,11 @@ import exceptions as e
 
 
 class Moneta():
-    nominal = [0, 0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 50]
+    """Klasa przechowująca monety akceptowane przez parkomat
+        lista nominal zawiera dopuszczalne nominały
+        lista drobne zawiera drobne pieniądze czyli te w monetach
+    """
+    nominal = [0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 50]
     drobne = [x for x in nominal if x <= 5]
 
     def __init__(self, wartosc):
@@ -16,19 +20,24 @@ class Moneta():
         else:
             raise AttributeError(f'"{wartosc}" nie jest akceptowanym nominałem')
     def get_wartosc(self):
+        """Funkcja zwraca wartosc danej monety"""
         return self.wartosc
     def get_nominal(self):
+        """Funkcja zwraca liste dostepnych nominałów"""
         return self.nominal
     def get_drobne(self):
+        """Funkcja zwraac listę drobnych"""
         return self.drobne
 
-class Skarbonka(Moneta):
+class Parkomat_pieniadze(Moneta):
+    """Klasa zajmuje obsługą pieniędzy w parkomacie. Dziedziczy po klasie Moneta"""
     def __init__(self, wartosc):
         super().__init__(wartosc)
         self.listaMonet=[]
         self.licznikMonet = []
 
     def dodajMonete(self, moneta, ile=1):
+        """Funckcja umożliwia wrzucenie monety do listy monet"""
         try:
             if len(self.licznikMonet) > 199:
                 raise e.PrzepelnienieDrobnych
@@ -45,9 +54,11 @@ class Skarbonka(Moneta):
             messagebox.showwarning("Zbyt dużo monet. Maksymalnie można wrzucić 200 monet", e.PrzepelnienieDrobnych.message)
 
     def budget(self):
+        """Funkcja zwraca aktualnie wrzuconą kwotę"""
         return sum([moneta.get_wartosc() for moneta in self.listaMonet])
 
     def oplaconyCzas(self):
+        """Funkcja konwertuje wrzucone pieniądze na ilość sekund parkowania"""
         if(self.budget() <= 2):
             return self.budget()*1800
         elif(self.budget()>2 and self.budget() <= 6):
@@ -56,24 +67,28 @@ class Skarbonka(Moneta):
             return 7200 + (self.budget()-6)*720
 
 
-class Czas(Skarbonka):
+class Parkomat_czas(Parkomat_pieniadze):
+    """Klasa zajmuje się zarządzaniem opłaconym czasem. Dziedziczy po klasie Parkomat_pieniadze"""
     def __init__(self, wartosc):
         super().__init__(wartosc)
         self.wyjazd = datetime.datetime.now()
         self.teraz = datetime.datetime.now()
 
     def ustaw_rano(self, data):
+        """Funkcja ustawia czas na godzinę 8 rano"""
         while True:
             data = data + timedelta(seconds=1)
             if data.hour == 8:
                 return data
 
     def ustaw_poniedzialek(self, data):
+        """Funkcja ustawia dzień tygodnia na poniedziałek"""
         while True:
             data = data + timedelta(days=1)
             if data.weekday() == 0:
                 return data
     def aktualizujCzas(self):
+        """Funkcja aktualizuje czas wyjazdu zgodnie z zapłaconą kwotą"""
         i = 0
         czas_wyjazdu = self.wyjazd
         while True:
@@ -94,13 +109,15 @@ class Czas(Skarbonka):
                 i += 1
         return czas_wyjazdu
     def nowy_czas(self, data):
+        """Funkcja zwraca czas z teraz"""
         self.teraz = data
         return data
 
 def czasFormat(data):
+    """Funckja służy do ustawiania daty i godziny w odpowiednim formacie"""
     pom = (str(data)).split(" ")
     czas = pom[0]
     pom = pom[1].split(":")
     czas = czas + " " + pom[0] + ":" + pom[1]
     return czas
-timer = Czas(1)
+parkomat = Parkomat_czas(1)

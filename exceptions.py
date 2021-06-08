539110f80ef32d6486dc66499c5b4e07efa846b9
MACIEKz8 <maciej.zawisz@student.pk.edu.pl>
2021-06-08 23:21:03 +0200

    Dodanie doc_strings

--- exceptions.py
+++ exceptions.py
@@ -4,19 +4,24 @@ class Error(Exception):
 
 
 class PrzepelnienieDrobnych(Error):
+    """Klasa wyjątku dla przepełnienia ilości drobnych w parkomacie"""
     message = "Parkomat przepelniony! Maksymalna ilość wrzuconych monet to 200."
 
 
 class RejestracjaMaleLitery(Error):
+    """Klasa wyjątku odpowiedzialna za niepoprawny numer rejestracyjny"""
     message = "Rejestracja moze skladac sie jedynie z duzych liter.\nWprowadz nr rejetracji samochodu jeszcze raz!"
 
 
 class NieWprowadzonoNumeruRejestracyjnego(Error):
+    """Klasa wyjątku odpowiedzialna za brak wprowadzonego numeru rejestracyjnego"""
     message = "Wprowadź numer rejestracyjny!"
 
 
 class NieWrzuconoMonet(Error):
+    """Klasa wyjątku odpowiedzialna za brak wrzuconej monety"""
     message = "Nie wrzucono żadnej monety!"
 
 class NiepoprawnyCzas(Error):
+    """Klasa odpowiedzialna za wyjątek podania niepoprawnej godziny"""
     message = "Podano niepoprawną godzinę!"
